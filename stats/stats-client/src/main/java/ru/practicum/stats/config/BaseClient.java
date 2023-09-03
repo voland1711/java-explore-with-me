13723db975087ec8fe45d17db8f1c53394547f91
ViktorBuzmakov <buzmak-1711@rambler.ru>
2023-09-04 01:59:06 +0500

    Внес исправления в BaseClient, внес дополнительные коррективы

--- stats/stats-client/src/main/java/ru/practicum/stats/config/BaseClient.java
+++ stats/stats-client/src/main/java/ru/practicum/stats/config/BaseClient.java
@@ -1,7 +1,6 @@
 package ru.practicum.stats.config;
 
 import org.springframework.http.*;
-import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
