2ea5316f632d03664cb577c840bb1a2290ac0c50
ViktorBuzmakov <buzmak-1711@rambler.ru>
2023-09-05 01:14:24 +0500

    Внес изменения в соотвествии с полученными замечаниями и предложениями

--- stats/stats-client/src/main/java/ru/practicum/stats/config/BaseClient.java
+++ stats/stats-client/src/main/java/ru/practicum/stats/config/BaseClient.java
@@ -5,7 +5,6 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 import java.util.Map;
 
 public class BaseClient {
@@ -54,5 +53,5 @@ public class BaseClient {
         }
         return prepareGatewayResponse(serverResponse);
     }
-    
+
 }
